221c65345f67bbf54ebffcf0478c66b468fa3dab
maria.helena.satyro.gomes.alves <mhsatyro@gmail.com>
2023-04-27 11:36:38 -0300

    sadgjasgd

--- src/Campeonato.java
+++ src/Campeonato.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Campeonato {
+import java.util.Objects;
+
+public class Campeonato {
+    private final Time[] times;
+    private final String nome;
+
+    public Campeonato(String nome, int maxTimes) {
+        this.times = new Time[maxTimes];
+        this.nome = nome;
+    }
+
+    public boolean addTime(Time time) {
+        return false;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Campeonato that = (Campeonato) o;
+        return nome.equalsIgnoreCase(that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome.toLowerCase());
+    }
 }
--- src/Time.java
+++ src/Time.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Time {
+import java.util.Objects;
+
+public class Time {
+    private final String id;
+    private final String nome;
+    private final String mascote;
+
+    public Time(String id, String nome, String mascote) {
+        this.id = id;
+        this.nome = nome;
+        this.mascote = mascote;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Time time = (Time) o;
+        return id.equals(time.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
